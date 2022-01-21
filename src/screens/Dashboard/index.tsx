3b3e77c07788b9c89e67d3b708fc7d712fcec7c9
Maurício de Sousa Carvalho <mauricio.carvalho@radixeng.com.br>
2022-01-21 19:55:55 -0300

    starting cards of transactions

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import { HighlightCard } from '../../components/HighlightCard';
 
-import { Container, Title } from './styles';
+import { 
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+} from './styles';
 
 export function Dashboard(){
   return(
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/33472260?v=4' }}/>
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Maurício</UserName>
+            </User>
+          </UserInfo>
+
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+
+      <HighlightCard />
     </Container>
   )
 }
