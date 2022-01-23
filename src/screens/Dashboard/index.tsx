0257e79a41a412cd32b12f9d11a2da0032d73fd8
Maurício de Sousa Carvalho <mauricio.carvalho@radixeng.com.br>
2022-01-23 18:42:49 -0300

    finishing transaction listing typing

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { HighlightCard } from '../../components/HighlightCard';
-import { TransactionCard } from '../../components/TransactionCard';
+import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
 
 import { 
   Container,
@@ -15,9 +15,46 @@ import {
   HighlightCards,
   Transactions,
   Title,
+  TransactionList,
 } from './styles';
 
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
+
 export function Dashboard(){
+  const data: DataListProps[] = [{
+    id: '1',
+    type: 'positive',
+    title: 'Desenvolvimento de site',
+    amount: '12.000,00',
+    category: {
+      name: 'Vendas',
+      icon: 'dollar-sign'
+    },
+    date: '13/04/2020'
+  },{
+    id: '2',
+    type: 'negative',
+    title: 'Hamburgueria Pizzy',
+    amount: '59,00',
+    category: {
+      name: 'Alimentação',
+      icon: 'coffee'
+    },
+    date: '13/04/2020'
+  },{
+    id: '3',
+    type: 'negative',
+    title: 'Aluguel',
+    amount: '2.000,00',
+    category: {
+      name: 'Casa',
+      icon: 'shopping-bag'
+    },
+    date: '13/04/2020'
+  }]
+
   return(
     <Container>
       <Header>
@@ -58,7 +95,12 @@ export function Dashboard(){
       <Transactions>
         <Title>Listagem</Title>
 
-        <TransactionCard />
+        <TransactionList 
+          data={data}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => <TransactionCard data={item} />}
+        />
+
       </Transactions>
     </Container>
   )
